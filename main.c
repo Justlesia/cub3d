190f863169f70f728a3757262d33139ace1dbfde
Elena Kiseleva <61661122+Justlesia@users.noreply.github.com>
2021-02-02 18:51:38 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 //#include "libft.a"
 #include "cub3d.h"
 # include "mlx_mms/mlx.h"
-#	include "libft/libft.h"
+//#	include "libft/libft.h"
 #	include "cub3d.h"
 #	include <stdlib.h>
 #	include <zconf.h>
 #	include <fcntl.h>
 //#include "libft/libft.h"
 //#	include "libft/libft.h"
-//#	include "cub3d.h"
+#	include "cub3d.h"
 //#	include <stdlib.h>
 ////#	include <unistd.h>
 ////#	include <stdio.h>
@@ -24,30 +24,77 @@ typedef struct  s_vars {
 }               t_vars;
 
 
-int             ft_close(int keycode, t_vars *vars)
+int             ft_action(int key, t_vars *vars)
 {
-	mlx_destroy_window(vars->mlx, vars->win);
+	//mlx_destroy_window(vars->mlx, vars->win);
+	printf("key-%d\n", key);
+	if (key == 12)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+	}
+	if (key == 1)
+	{
+		mlx_pixel_put(vars->mlx, vars->win, 50, 50, 0xFFFFFF);
+	}
+	if (key == 8)
+	{
+		mlx_clear_window(vars->mlx, vars->win);
+	}
+
+//	if (key == 0)
+//		mlx_pixel_put(vars->mlx, vars->win, 50,50, 0xFFFFFF);
+//	if (key == 1)
+//		mlx_destroy_window(vars->mlx, vars->win);
 	return(0);
 }
+//
+//[ cos(a) -sin(a) ]
+//[ sin(a)  cos(a) ]
+
+//int deal_key(int key, void *param)
+//	{
+//		write(1,"X",1);
+//		//mlx_pixel_put(mlx_ptr, win_ptr )
+//		mlx_pixel_put(vars->mlx_ptr, vars->win_ptr, 25,50, 0xFFFFFF);
+//		return (0);
+//}
+int     render_next_frame(void *YourStruct);
 
 int             main(void)
 {
-	t_vars    vars;
+	t_vars vars;
 
 	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
-	mlx_hook(vars.win, 2, 1L<<0, ft_close, &vars);
+	vars.win = mlx_new_window(vars.mlx, 100, 100, "Hello world!");
+	//mlx_hook(vars.win, 2, 1L << 0, ft_close, &vars);
+	//mlx_pixel_put(vars.mlx, vars.win, 50,50, 0xFFFFFF);
+
+	int length;
+	int width;
+
+
+	int n = 20;
+
+	length = 0;
+	width = 0;
+	while (length < n)
+	{
+		while (width < n)
+		{
+			mlx_pixel_put(vars.mlx, vars.win, 40 + length, 40 + width, 0xFFFFFF);
+			mlx_pixel_put(vars.mlx, vars.win, 40 + width, 40 + length, 0x00FF00);
+			mlx_pixel_put(vars.mlx, vars.win,  40 + n - length , 40 + n- width, 0xFFFFFF);
+			mlx_pixel_put(vars.mlx, vars.win, 40 + n- width, 40 + n- length, 0x00FF00);
+			width++;
+			}
+		length++;
+	}
+
+	mlx_hook(vars.win, 2, 1L << 0, ft_action, &vars);
+	//mlx_hook(mlx, 2, 0, &func, param);
+	//mlx_loop_hook(mlx, render_next_frame, YourStruct);
 	mlx_loop(vars.mlx);
-
-
-int deal_key(int key, void *param)
-{
-	write(1,"X",1);
-	//mlx_pixel_put(mlx_ptr, win_ptr )
-	mlx_pixel_put(param->mlx_ptr, param->win_ptr, 25,50, 0xFFFFFF);
-	return (0);
 }
-
 
 //int main(void)
 //{
@@ -80,5 +127,3 @@ int deal_key(int key, void *param)
 
 
 //gcc main.c parcer.c libft.a ./mlx_mms/libmlx.dylib  -framework OpenGL -framework AppKit
-
-
